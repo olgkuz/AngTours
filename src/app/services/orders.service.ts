493bf3d1323a961804540a859bbdb9eb0ed0c670
olgKuz <kuznecovao@list.ru>
2025-04-24 21:01:10 +0300

    change order to orders

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { IOrder } from '../models/order';
+import { IOrder } from '../models/orders';
 import { API } from '../shared/api';
 
 @Injectable({
